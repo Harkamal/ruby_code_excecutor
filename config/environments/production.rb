65279167edb4645e45334657cf894b883f0a6266
unknown <harkamalsingh@HarkamlSingh.Nagarro.local>
2014-08-23 21:32:37 +0530

    updates for this commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,11 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+  # heroku configuration
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
