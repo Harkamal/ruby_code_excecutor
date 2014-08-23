8303fb225fb959c6de2eb4cafbd09fa8a4464d54
unknown <harkamalsingh@HarkamlSingh.Nagarro.local>
2014-08-23 21:51:03 +0530

    k

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
+  config.action_dispatch.x_sendfile_header = nil
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
