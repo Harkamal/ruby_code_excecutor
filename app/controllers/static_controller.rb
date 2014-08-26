ce130eb72550dae6d0a56d633be77258b4581948
unknown <harkamalsingh@HarkamlSingh.Nagarro.local>
2014-08-26 06:57:46 +0530

    exception on timeout error.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -21,8 +21,11 @@ class StaticController < ApplicationController
 	end
 	
 	def create_and_execute_ruby_file
-	  doc = "begin\n"
+	  doc = "require 'timeout'\n" unless params[:export]
+	  doc << "begin\n"
+	  doc << "Timeout.timeout(5) do\n" unless params[:export]
     doc << params[:rubyCode] + "\n"
+    doc << "end\n" unless params[:export]
     doc << "rescue Exception => e \n"
     doc << 'puts "Error: #{e.class} - " + e.to_s'
     doc << "\nend"
@@ -30,7 +33,14 @@ class StaticController < ApplicationController
     unless (`ruby -c cc.rb`).include?("OK")
       return @data = "Syntax error, Provide correct Ruby syntax."
     end
-    @data = `ruby cc.rb`
+    begin
+      Timeout.timeout(3) do
+        @data = `ruby cc.rb` 
+      end
+    rescue Exception => e
+      @data = "Error: #{e.class} - " + e.to_s
+      puts "exception==================#{e.to_s}"
+    end
 	end
 
 	def export
